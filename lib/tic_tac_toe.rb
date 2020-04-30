f9b1906fe0119c7e6d8e13ae2806728f1b402dfa
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-30 07:52:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,7 +5,7 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
   
- def WIN_COMBINATIONS = [
+ WIN_COMBINATIONS = [
    [0,1,2],
    [3,4,5],
    [6,7,8],
@@ -15,7 +15,7 @@ class TicTacToe
    [0,4,8],
    [2,4,6]
    ]
- end
+
   
   
   
