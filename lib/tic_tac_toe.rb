da3a195220945fcb6f542c4acd0b4d2c4bbe3685
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-30 07:55:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,7 @@ class TicTacToe
    [2,4,6]
    ]
 
-  def self.display_board(board)
+  def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
